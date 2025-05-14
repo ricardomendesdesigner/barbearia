14f77c63bedfe0c80fb1461e01cccdb8fb8562b6
pauloricardodev <prmendes32@gmail.com>
2025-05-13 23:01:56 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -6,7 +6,6 @@ import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
 import Link from "next/link"
 import Image from "next/image"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import {
   Dialog,
   DialogContent,
