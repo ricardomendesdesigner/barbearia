c0f866a72c0ca3e2d5d20094f3d52c7a6aba297f
pauloricardodev <prmendes32@gmail.com>
2025-06-03 00:03:08 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { ptBR } from "date-fns/locale"
+import { format } from "date-fns"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -26,7 +28,6 @@ const Home = async () => {
           date: {
             gte: new Date(),
           }, // Filtra agendamentos futuros
-          
         },
         include: {
           service: {
@@ -37,7 +38,7 @@ const Home = async () => {
         },
         orderBy: {
           date: "asc",
-        }
+        },
       })
     : []
 
@@ -47,8 +48,15 @@ const Home = async () => {
       <Header />
       {/*Banner*/}
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Paulo Ricardo!</h2>
-        <p>Segunda-feira, 05 de Agosto.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "bem vindo"}!
+        </h2>
+
+        <p>
+        <span className="capitalize">{format(new Date(), "EEEE, dd", { locale: ptBR })}</span>
+        <span>&nbsp; de &nbsp;</span> 
+        <span className="capitalize">{format(new Date(), "MMMM", { locale: ptBR })}</span>
+        </p>
 
         {/* Busca */}
         <div className="mt-6">
